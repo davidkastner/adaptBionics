24158d6cd99e5c7fddbbc0244026800fbe0c9de5
David Kastner <kastner@mit.edu>
2020-05-24 22:09:16 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,9 +39,9 @@
         /**
          *  Page Loader
          **/
-        // setTimeout(function () {
-        //     $('.page-loader').addClass('load-complete');
-        // }, 0);
+        setTimeout(function () {
+            $('.page-loader').addClass('load-complete');
+        }, 1500);
 
         /**
          *  Parallax with Scrollax.js - Initialization
